0abb518ce07dac590f522ba7e9725ed5ce022972
Frank Hou <93571148+frankhou42@users.noreply.github.com>
2026-03-30 09:19:16 -0500

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,24 @@
+"""
+The  valid substring is 
+(length of substring - mostFreqCharCount) <= k
+
+build global hashmap and remove elements and find max as we go
+"""
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        hashmap = {}
+        mostFreq = 0
+        maxSubstring = 0
+        for r in range(len(s)):
+            hashmap[s[r]] = hashmap.get(s[r], 0) + 1
+            mostFreq = max(mostFreq, hashmap[s[r]])
+            while (r - l + 1 - mostFreq) > k:
+                #first decrement hashmap then move l ptr
+                hashmap[s[l]] -= 1
+                l += 1
+            
+            maxSubstring = max(maxSubstring, r - l + 1)
+        
+        return maxSubstring
+            
