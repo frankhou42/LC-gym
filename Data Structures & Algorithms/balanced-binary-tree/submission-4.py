1d42e42c97a7a8d2e8f780822524f82e2004947e
Frank Hou <93571148+frankhou42@users.noreply.github.com>
2026-03-30 17:24:32 -0500

    Add: balanced-binary-tree - submission-4

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-4.py
@@ -0,0 +1,34 @@
+"""
+Recursive case:
+at arbitrary node, its balanced if its left height - right height <= 1
+in abs val, left and right subtree are balanced.
+Hence we need information on height of left and right and the truth val
+of left and right
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return [True, 0]
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            #subtree and currNode balanced
+            if abs(left[1] - right[1]) <= 1 and left[0] and right[0]:
+                return [True, 1 + max(left[1], right[1])]
+            else:
+                return [False, 1 + max(left[1], right[1])]
+
+
+        return dfs(root)[0]
+
+
+            
