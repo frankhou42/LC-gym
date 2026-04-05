e2b0a26c3e900b311c735d5f8ba0cbc69adb39e5
Frank Hou <93571148+frankhou42@users.noreply.github.com>
2026-04-04 19:16:53 -0500

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.py
@@ -0,0 +1,60 @@
+"""
+Sol:
+skip the number to not use it and all its dups or use the number and go to next
+
+Key Insight:
+Backtracking base cases = success + failure stop condition
+Tree base cases = structure ends
+
+Backtracking has no left and right subtree to recurse over
+we have to define the state of what left and right subtree
+means. (e.g. left subtree in this problem is to skip number)
+
+For each choice:
+    we change the state
+    explore all possibilities from that choice
+    then restore the original state before trying the next choice
+
+sort to avoid dups by skipping dups when we decide to not use a number
+
+we are trying to get to the leaf node which is the possibilities
+"""
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        combination = []
+        def dfs(total, index):
+            #Base cases: success and failure
+            if total == target:
+                res.append(combination.copy())
+                return
+            
+            if index >= len(candidates) or total > target:
+                return
+
+            #Recursive case
+            #Choice 1:
+            next_index = index
+            while next_index < len(candidates) - 1 and candidates[next_index + 1] == candidates[next_index]:
+                next_index += 1
+            dfs(total, next_index + 1)
+            
+
+
+            #Choice 2:
+            combination.append(candidates[index])
+            total += candidates[index]
+            dfs(total, index + 1)
+            #need to undo after finished exploring that path
+
+            combination.pop()
+            total -= candidates[index]
+
+        dfs(0, 0)
+        return res
+
+#time complexity O(2^n * n) since there are 2^n probabilities (leaf nodes), 
+#at each leaf node we do  O(n) opeartion to do a deep copy. 
+#space complexity O(n) since there are 
