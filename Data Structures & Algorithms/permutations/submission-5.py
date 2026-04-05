6b64bc4ae0151af52737314df58f6bf49c3f47b3
Frank Hou <93571148+frankhou42@users.noreply.github.com>
2026-04-04 23:32:28 -0500

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,38 @@
+"""
+at each step we choose a number that we haven't used yet
+and add it to the local result (recursive case). We recursively find all permutations
+if we reach full length append to global result (base case). 
+
+Key Insight:
+The core idea of dfs is to explore on path fully before trying another path
+The base case tells us when does a path end.
+ 
+"""
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        permutation = []
+        
+        def dfs():
+            #base case
+            if len(permutation) == len(nums):
+                res.append(permutation.copy())
+                return
+            
+            #recursive case
+            for num in nums:
+                if num not in permutation:
+                    permutation.append(num)
+                    dfs()
+                    permutation.pop()
+            
+        
+        dfs()
+        return res
+
+#n is depth of tree
+#time complexity: O(n! x n) n! is tree size
+#space complexity: O(n)
+
+            
